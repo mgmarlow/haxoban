f532aeef90f3746c1cb278983b5f39b673df3283
Graham Marlow <mgmarlow@hey.com>
2021-03-01 18:22:10 -0800

    Use the actions API

--- source/Player.hx
+++ source/Player.hx
@@ -2,15 +2,37 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.input.actions.FlxAction.FlxActionAnalog;
+import flixel.input.actions.FlxAction.FlxActionDigital;
+import flixel.input.actions.FlxActionInputDigital.FlxActionInputDigitalKeyboard;
+import flixel.input.actions.FlxActionManager;
+import flixel.input.actions.FlxActionSet;
 import flixel.util.FlxColor;
 
 class Player extends FlxSprite
 {
 	static inline var SIZE:Int = 64;
 
+	var actionManager = new FlxActionManager();
+
+	var up = new FlxActionDigital();
+	var down = new FlxActionDigital();
+	var left = new FlxActionDigital();
+	var right = new FlxActionDigital();
+
 	public function new(x:Float = 0, y:Float = 0)
 	{
 		super(x, y);
+
+		// Maybe throw this somewhere else?
+		FlxG.inputs.add(actionManager);
+
+		up.addKey(W, JUST_RELEASED);
+		down.addKey(S, JUST_RELEASED);
+		left.addKey(A, JUST_RELEASED);
+		right.addKey(D, JUST_RELEASED);
+
+		actionManager.addActions([up, down, left, right]);
 		makeGraphic(SIZE, SIZE, FlxColor.BLUE);
 	}
 
@@ -22,25 +44,18 @@ class Player extends FlxSprite
 
 	function updateMovement()
 	{
-		var dir = {x: 0, y: 0};
+		if (up.triggered)
+			doMove({x: 0, y: -1});
+		else if (down.triggered)
+			doMove({x: 0, y: 1});
+		else if (left.triggered)
+			doMove({x: -1, y: 0});
+		else if (right.triggered)
+			doMove({x: 1, y: 0});
+	}
 
-		if (FlxG.keys.anyJustReleased([UP, W]))
-		{
-			dir.y = -1;
-		}
-		else if (FlxG.keys.anyJustReleased([DOWN, S]))
-		{
-			dir.y = 1;
-		}
-		else if (FlxG.keys.anyJustReleased([LEFT, A]))
-		{
-			dir.x = -1;
-		}
-		else if (FlxG.keys.anyJustReleased([RIGHT, D]))
-		{
-			dir.x = 1;
-		}
-
+	function doMove(dir:{x:Int, y:Int})
+	{
 		var nextX = x + (dir.x * SIZE);
 		var nextY = y + (dir.y * SIZE);
 
@@ -49,11 +64,8 @@ class Player extends FlxSprite
 			return;
 		}
 
-		if (dir.x != 0 || dir.y != 0)
-		{
-			x = nextX;
-			y = nextY;
-		}
+		x = nextX;
+		y = nextY;
 	}
 
 	function outsideBounds(nextX:Float, nextY:Float)
