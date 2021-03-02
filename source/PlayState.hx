23bb9e7e5b6ab211f3e9ead995464fc99198e1d1
Graham Marlow <mgmarlow@hey.com>
2021-03-01 19:06:55 -0800

    Load tilemap

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,14 +1,28 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxState;
+import flixel.addons.editors.ogmo.FlxOgmo3Loader;
+import flixel.tile.FlxTilemap;
 
 class PlayState extends FlxState
 {
 	var player:Player;
+	var map:FlxOgmo3Loader;
+	var walls:FlxTilemap;
 
 	override public function create()
 	{
-		player = new Player(0, 0);
+		map = new FlxOgmo3Loader(AssetPaths.haxoban__ogmo, AssetPaths.room_001__json);
+		walls = map.loadTilemap(AssetPaths.sokoban_tilesheet__png, "walls");
+		walls.setTileProperties(89, FlxObject.NONE);
+		walls.setTileProperties(19, FlxObject.ANY);
+		add(walls);
+
+		player = new Player();
+		map.loadEntities(placeEntities, "entities");
+
 		add(player);
 		super.create();
 	}
@@ -16,5 +30,15 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		FlxG.collide(player, walls);
+	}
+
+	function placeEntities(entity:EntityData)
+	{
+		if (entity.name == "player")
+		{
+			player.setPosition(entity.x, entity.y);
+		}
 	}
 }
